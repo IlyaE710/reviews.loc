291f5878d5ba1df2b087960235f2f29a64725bfc
IlyaE <ilyaermakov710@ya.ru>
2022-11-13 20:29:45 +0300

    Create simple router

---
+++ Core/Routing/Router.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Core\Routing;
+
+class Router
+{
+    private array $routes;
+    private const GET = "GET";
+    private const POST = "POST";
+
+    public function get(string $uri, array $callback) : void
+    {
+        $this->addRoute(self::GET, $uri, $callback);
+    }
+
+    public function post(string $uri, array $callback) : void
+    {
+        $this->addRoute(self::POST, $uri, $callback);
+    }
+
+    private function addRoute(string $method, string $uri, array $callback)
+    {
+        $this->routes[$method . "@" . $uri] = $callback;
+    }
+
+    public function dispatch() : void
+    {
+        print_r($this->routes);
+    }
+}
--- Public/index.php
+++ Public/index.php
@@ -1,5 +1,11 @@
 <?php
 
+use Core\Routing\Router;
+
 require_once __DIR__ . "/../vendor/autoload.php";
 
-include "../Templates/main.php";
+$router = new Router();
+$router->get('/', ["controller", "action"]);
+$router->get('sf/sd', ["controller1", "action"]);
+$router->post('sf/sd', ["controller1", "action"]);
+$router->dispatch();
