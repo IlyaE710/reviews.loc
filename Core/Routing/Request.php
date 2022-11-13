9af76bdee7c618ee1235ff15f2208ee087c775f7
IlyaE <ilyaermakov710@ya.ru>
2022-11-13 20:33:09 +0300

    Add class Request

---
+++ Core/Routing/Request.php
@@ -0,0 +1,35 @@
+<?php
+
+namespace Core\Routing;
+
+class Request
+{
+    public string $method;
+    public array $uri;
+    public string $path;
+    public array $params;
+
+    public function __construct()
+    {
+        $this->method = $_SERVER['REQUEST_METHOD'];
+        $this->uri = parse_url($_SERVER['REQUEST_URI']);
+        $this->path = $this->uri['path'];
+        $this->params = $this->parseQuery();
+    }
+
+    private function parseQuery(): array
+    {
+        $query = $this->uri['query'];
+        $queries = explode('&', $query);
+        if (empty($queries))
+        {
+            return [];
+        }
+        $params = [];
+        foreach ($queries as $query) {
+            [$k, $v] = (explode('=', $query));
+            $params[$k] = $v;
+        }
+        return $params;
+    }
+}
